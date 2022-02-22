7b98378e7207093486ff20d2a73ef31ac197f0d0
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-02-22 15:51:15 -0700

    add brewcrew to portfolio

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,137 +1,158 @@
-import React from "react";
+import React from 'react';
 
 function Projects() {
 	return (
 		<>
-			<div className="container">
-				<div className="row">
-					<div className="col-12 col-md-8 col-lg-10">
-						<div className="container mt-5 p-4 content">
+			<div className='container'>
+				<div className='row'>
+					<div className='col-12 col-md-8 col-lg-10'>
+						<div className='container mt-5 p-4 content'>
 							<h1>Portfolio</h1>
 							<hr />
-							<div className="row">
-								<div className="col-12 px-1">
-									<div className="row p-2">
-										<div className="col-12 col-lg-6 mb-3">
+							<div className='row'>
+								<div className='col-12 px-1'>
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/wavmovers-ss.png"
-												alt="wavmovers"
-												className="portfolio-ss"
+												src='assets/images/brewcrew-ss.png'
+												alt='brewcrew'
+												className='portfolio-ss'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
+											<br />
+											<h4>BrewCrew</h4>
+											<a href='https://lukeoxner.github.io/brew-crew/'>
+												lukeoxner.github.io/brew-crew
+											</a>
+											<br />
+											<a href='https://github.com/lukeoxner/brew-crew'>
+												github.com/lukeoxner/brew-crew
+											</a>
+										</div>
+										<hr />
+									</div>
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
+											<img
+												src='assets/images/wavmovers-ss.png'
+												alt='wavmovers'
+												className='portfolio-ss'
+											/>
+										</div>
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>WavMovers (Capstone Project)</h4>
-											<a href="https://wavmovers.herokuapp.com/">
+											<a href='https://wavmovers.herokuapp.com/'>
 												wavmovers.herokuapp.com
 											</a>
 											<br />
-											<a href="https://github.com/Jdouville8/Audio-Sample-Sharing-Service">
+											<a href='https://github.com/Jdouville8/Audio-Sample-Sharing-Service'>
 												github.com/Jdouville8/WavMovers
 											</a>
 										</div>
 										<hr />
 									</div>
-									<div className="row p-2">
-										<div className="col-12 col-lg-6 mb-3">
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/brawndo-ss.png"
-												alt="brawndo"
-												className="portfolio-ss"
+												src='assets/images/brawndo-ss.png'
+												alt='brawndo'
+												className='portfolio-ss'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>Brawndo (Group Project 2)</h4>
-											<a href="https://brawndoplant.herokuapp.com/">
+											<a href='https://brawndoplant.herokuapp.com/'>
 												brawndoplant.herokuapp.com
 											</a>
 											<br />
-											<a href="https://github.com/Tevvels/Brawndo">
+											<a href='https://github.com/Tevvels/Brawndo'>
 												github.com/Tevvels/Brawndo
 											</a>
 										</div>
 										<hr />
 									</div>
-									<div className="row p-2">
-										<div className="col-12 col-lg-6 mb-3">
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/employee-ss.png"
-												alt="directory"
-												className="portfolio-ss"
+												src='assets/images/employee-ss.png'
+												alt='directory'
+												className='portfolio-ss'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>Employee Directory</h4>
-											<a href="https://lukeoxner.github.io/employee-directory/">
+											<a href='https://lukeoxner.github.io/employee-directory/'>
 												lukeoxner.github.io/employee-directory
 											</a>
 											<br />
-											<a href="https://github.com/lukeoxner/employee-directory">
+											<a href='https://github.com/lukeoxner/employee-directory'>
 												github.com/lukeoxner/employee-directory
 											</a>
 										</div>
 										<hr />
 									</div>
-									<div className="row p-2">
-										<div className="col-12 col-lg-6 mb-3">
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/budget-ss.png"
-												className="portfolio-ss"
-												alt="budget"
+												src='assets/images/budget-ss.png'
+												className='portfolio-ss'
+												alt='budget'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>Budget Tracker</h4>
-											<a href="https://stormy-garden-25212.herokuapp.com/">
+											<a href='https://stormy-garden-25212.herokuapp.com/'>
 												budget-tracker.herokuapp.com
-											</a>{" "}
+											</a>{' '}
 											<br />
-											<a href="https://github.com/lukeoxner/budget-tracker">
+											<a href='https://github.com/lukeoxner/budget-tracker'>
 												github.com/lukeoxner/budget-tracker
 											</a>
 										</div>
 										<hr />
 									</div>
-									<div className="row p-2">
-										<div className="col-12 col-lg-6 mb-3">
+									<div className='row p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/weather-ss.png"
-												alt="weather"
-												className="portfolio-ss"
+												src='assets/images/weather-ss.png'
+												alt='weather'
+												className='portfolio-ss'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>Weather Dashboard</h4>
-											<a href="https://lukeoxner.github.io/weather-dashboard/">
+											<a href='https://lukeoxner.github.io/weather-dashboard/'>
 												lukeoxner.github.io/weather-dashboard
 											</a>
 											<br />
-											<a href="https://github.com/lukeoxner/weather-dashboard">
+											<a href='https://github.com/lukeoxner/weather-dashboard'>
 												github.com/lukeoxner/weather-dashboard
 											</a>
 										</div>
 										<hr />
 									</div>
-									<div className="row mt-3 p-2">
-										<div className="col-12 col-lg-6 mb-3">
+									<div className='row mt-3 p-2'>
+										<div className='col-12 col-lg-6 mb-3'>
 											<img
-												src="assets/images/workday-ss.png"
-												alt="scheduler"
-												className="portfolio-ss"
+												src='assets/images/workday-ss.png'
+												alt='scheduler'
+												className='portfolio-ss'
 											/>
 										</div>
-										<div className="col-12 col-lg-6 mb-3">
+										<div className='col-12 col-lg-6 mb-3'>
 											<br />
 											<h4>Workday Scheduler</h4>
-											<a href="https://lukeoxner.github.io/workday-scheduler/">
+											<a href='https://lukeoxner.github.io/workday-scheduler/'>
 												lukeoxner.github.io/workday-scheduler
-											</a>{" "}
+											</a>{' '}
 											<br />
-											<a href="https://github.com/lukeoxner/workday-scheduler">
+											<a href='https://github.com/lukeoxner/workday-scheduler'>
 												github.com/lukeoxner/workday-scheduler
 											</a>
 										</div>
